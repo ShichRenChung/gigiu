2fbe5b031e4a53cb5bc35783ff4fb7b390b4b9e3
ShichRenChung <47464941+ShichRenChung@users.noreply.github.com>
2021-04-29 20:42:17 +0800

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60562a8336274f001fc8d74f",
+				"60562a8336274f001fc8d751"
+			]
+		},
+		"60562a8336274f001fc8d74f": {
+			"id": "60562a8336274f001fc8d74f",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60562a8336274f001fc8d751": {
+			"id": "60562a8336274f001fc8d751",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
